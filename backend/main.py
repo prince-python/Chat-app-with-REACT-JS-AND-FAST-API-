b077ba073242da62c8e5331a7925b4534acd67e6
kali <kali@kali>
2023-06-06 13:05:03 +0530

    project complete

--- backend/main.py
+++ backend/main.py
@@ -1,64 +1,63 @@
-from fastapi import FastAPI, WebSocket
+from typing import List
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
-import json 
+import json
 
-
-
-app=FastAPI()
-
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_method=["*"],
-    allow_headers=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
-
-
 class ConnectionManager:
-    def __init__(self) -> None:
-        self.active_connections:List[webSocket]=[]
-    async def connect(self,websocket:WebSocket):
-        await websocket.accept()
-        self.active_conections.append(webSocket)
-    def disconnect(self,websocket:WebSocket):
-        self.active_conections.remove(websocket)
-    async def send_persnol_message(self,message:str,websocket:WebSocket):
-            await websocket.send_text(message)
-            
-    async def broadcast(self,message:str):
-        for connection in self.active_conections:
-            await connection.send_text(message)
-            
-            
-            
-manager=ConnectionManager()
-            
 
+    def __init__(self) -> None:
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+# define endpoint
 
 
 @app.get("/")
-def home():
-    return {"message":"Hello World"}
-
-
+def Home():
+    return "Welcome home"
 
 
 @app.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket:WebSocket,client_id:int):
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
-    now=datetime.now()
-    currency_time=now.strftime("%H:%M")
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
     try:
-        while True :
+        while True:
             data = await websocket.receive_text()
-            message={"time":currency_time,"client_id":client_id,"message":data}
+            # await manager.send_personal_message(f"You wrote: {data}", websocket)
+            message = {"time":current_time,"clientId":client_id,"message":data}
             await manager.broadcast(json.dumps(message))
+            
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        message={"time":currency_time,"client_id":client_id,"message":data}
+        message = {"time":current_time,"clientId":client_id,"message":"Offline"}
         await manager.broadcast(json.dumps(message))
